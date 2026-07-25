3b2b64a1b0a7cb1ac50652efb0fc58601a891a5d
Ayush Kumar Maurya <ayushkrm48@gmail.com>
2026-07-25 19:15:22 +0530

    maine yaha change kiya hain

--- 01vitereact/src/test.jsx
+++ 01vitereact/src/test.jsx
@@ -3,4 +3,11 @@ function Test(){
         <p>This is for test</p>
     )
 }
+function Test2(){
+    return(
+        <p>
+            this is another testing
+        </p>
+    )
+}
 export default Test
