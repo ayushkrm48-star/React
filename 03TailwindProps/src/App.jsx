85720eb9c466b3be02d773af9e9c228d85e300bb
Ayush Kumar Maurya <ayushkrm48@gmail.com>
2026-07-26 02:59:57 +0530

    React Day 3

---
+++ 03TailwindProps/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from '/Users/sumitkumarmaurya/React/03TailwindProps/components/Card.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <div id="box">
+      <Card userName ="Ayush Kumar Maurya" btnText ="Click Here"/>
+
+      <Card userName="Sumit Kumar Maurya" btnText="Read More" />
+      </div>
+     
+    </>
+  )
+}
+
+export default App
